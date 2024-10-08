efe4cdbd32735c4794d5b67c31ebe602ffcbf408
Marcelo Arias <hello@marceloarias.com>
2024-10-08 00:17:54 -0500

    Update host main

--- api/run.py
+++ api/run.py
@@ -5,4 +5,4 @@ app = create_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(debug=False, port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
